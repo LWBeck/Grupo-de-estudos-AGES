ac72767baa838fc470a27c2793ab863508202963
LWBeck <l.beck02@edu.pucrs.br>
2026-03-16 16:43:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let x = 0;
+let y = 0;
+
+function somar(x,y){
+    return x + y;
+}
+
+function subtrair(x,y){
+    return x - y;
+}
+
+function multiplicar(x,y){
+    return x * y;
+}
+
+function dividir(x,y){
+    return x / y;
+}
