c465d8c75f29a8353d8e8cf4780e4cfee3e07481
LWBeck <l.beck02@edu.pucrs.br>
2026-03-17 16:56:22 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,70 @@
-let x = 0;
-let y = 0;
+const numpad = document.querySelectorAll('.num');
+const ops = document.querySelectorAll('.op');
 
-function somar(x,y){
-    return x + y;
+//const soma = document.getElementById("soma");
+//const sub = document.getElementById("sub");
+//const mult = document.getElementById("mult");
+//const div = document.getElementById("div");
+const eq = document.getElementById("eq");
+
+let expressao = []
+
+let lock = false;
+
+console.log("numpad: ", numpad);
+console.log("ops: ", ops);
+
+function replaceAt(originalString, index, replacementChar) {
+  return originalString.slice(0, index) + replacementChar + originalString.slice(index + 1);
 }
 
-function subtrair(x,y){
-    return x - y;
+function clique_num(){
+    numpad.forEach(button => {
+        button.addEventListener("click", function() {
+            if (tela.innerText == 0 || lock == true){
+                console.log("num: " + button.innerText);
+                lock = false;
+                cancelIdleCallback.
+                expressao.push(button.innerText);
+                //let display = button.innerText;
+                let display = expressao.join("");
+                tela.innerText = display;
+            }
+            else {
+                console.log("num: " + button.innerText);
+                expressao.push(button.innerText);
+                console.log(expressao)
+                //let display = button.innerText;
+                let display = expressao.join("");
+                tela.innerText = display;
+                //tela.innerText = tela.innerText + button.innerText;
+            }
+        }   )
+    })
 }
 
-function multiplicar(x,y){
-    return x * y;
+
+
+function clique_op(){
+    ops.forEach(button => {
+        console.log("op");
+        button.addEventListener('click', function() {
+            //tela.innerText = button.innerText;
+            if (tela.innerText.endsWith(1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9 || 0)){
+                tela.innerText = tela.innerText + button.innerText;
+                //tela.innerText = display;
+            }
+            else {
+                //let display = replaceAt(tela.innerText, tela.innerText.length-1, button.innerText);
+                tela.innerText = replaceAt(tela.innerText, tela.innerText.length-1, button.innerText);
+            }
+           
+})})
 }
 
-function dividir(x,y){
-    return x / y;
+function equal(){
+    let res = eval(tela.innerText);
+    tela.innerText = res;
+    lock = true;
+    console.log(lock)
 }
