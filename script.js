57107c2783b1e040beabb69875016dc19c369c2d
LWBeck <l.beck02@edu.pucrs.br>
2026-03-19 16:56:35 -0300

    Add files via upload
    almost finished

--- script.js
+++ script.js
@@ -1,70 +1,55 @@
 const numpad = document.querySelectorAll('.num');
 const ops = document.querySelectorAll('.op');
-
-//const soma = document.getElementById("soma");
-//const sub = document.getElementById("sub");
-//const mult = document.getElementById("mult");
-//const div = document.getElementById("div");
 const eq = document.getElementById("eq");
-
-let expressao = []
-
-let lock = false;
+const eg = document.getElementById("egg")
 
 console.log("numpad: ", numpad);
 console.log("ops: ", ops);
 
+let lock = false;
+console.log("lock: "+lock);
+
 function replaceAt(originalString, index, replacementChar) {
-  return originalString.slice(0, index) + replacementChar + originalString.slice(index + 1);
+    return originalString.slice(0, index) + replacementChar + originalString.slice(index + 1);
 }
 
-function clique_num(){
-    numpad.forEach(button => {
-        button.addEventListener("click", function() {
-            if (tela.innerText == 0 || lock == true){
-                console.log("num: " + button.innerText);
-                lock = false;
-                cancelIdleCallback.
-                expressao.push(button.innerText);
-                //let display = button.innerText;
-                let display = expressao.join("");
-                tela.innerText = display;
-            }
-            else {
-                console.log("num: " + button.innerText);
-                expressao.push(button.innerText);
-                console.log(expressao)
-                //let display = button.innerText;
-                let display = expressao.join("");
-                tela.innerText = display;
-                //tela.innerText = tela.innerText + button.innerText;
-            }
-        }   )
-    })
-}
+numpad.forEach(button => {
+    button.addEventListener("click", function() {
+        if (tela.innerText == "0" || lock == true){
+            console.log("num: " + button.innerText);
+            lock = false;
+            console.log("lock: "+lock);
+            //let display = button.innerText;
+            tela.innerText = button.innerText;
+        }
+        else {
+            console.log("num: " + button.innerText);
+            //let display = tela.innerText + button.innerText;
+            tela.innerText = tela.innerText + button.innerText;
+        }
+    }   )
+})
 
-
-
-function clique_op(){
-    ops.forEach(button => {
-        console.log("op");
-        button.addEventListener('click', function() {
-            //tela.innerText = button.innerText;
-            if (tela.innerText.endsWith(1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9 || 0)){
+ops.forEach(button => {
+    console.log("op");
+    button.addEventListener('click', function() {
+        switch (tela.innerText.at(-1)){
+            case "1": case "2": case "3": case "4": case "5": case "6": case "7": case "8": case "9": case "0":
+                console.log("op add");
                 tela.innerText = tela.innerText + button.innerText;
-                //tela.innerText = display;
-            }
-            else {
-                //let display = replaceAt(tela.innerText, tela.innerText.length-1, button.innerText);
+                lock = false;
+           
+            default:
+                console.log("op replace");
                 tela.innerText = replaceAt(tela.innerText, tela.innerText.length-1, button.innerText);
-            }
+                lock = false;
+        }
            
 })})
-}
 
 function equal(){
     let res = eval(tela.innerText);
     tela.innerText = res;
     lock = true;
-    console.log(lock)
+    console.log("lock: "+lock);
 }
